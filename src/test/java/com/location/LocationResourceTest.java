b46018f3297e35d87c04312a80e7a5c20e390213
Bhasker Reddy <breddy@tavisca.com>
2019-06-04 15:48:15 +0530

    Test case added for filter Venues

--- src/test/java/com/location/LocationResourceTest.java
+++ src/test/java/com/location/LocationResourceTest.java
@@ -1,6 +1,8 @@
 package com.location;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
@@ -10,7 +12,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.location.controller.LocationResource;
 
@@ -29,13 +30,32 @@ public class LocationResourceTest {
 				.andExpect(status().isOk())
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
 				
-				//check if the jason node exsits
+				//check if the json node exists
                 .andExpect(jsonPath("$.meta").exists())
                 .andExpect(jsonPath("$.response").exists())
                 .andExpect(jsonPath("$.response.venues").exists())
                 .andExpect(jsonPath("$.response.venues").isArray())
                 .andExpect(jsonPath("$.response.venues[0].name").value("KFC"))
-                .andExpect(jsonPath("$.response.venues[0].venueDetails").exists());
+                .andExpect(jsonPath("$.response.venues[0].venueDetails").exists())
+                .andExpect(jsonPath("$.response.venues[0].venueDetails.response").isEmpty());
+	}
+	
+	@Test
+	public void givenSearchParams_whenFilterVenues_thenVenuesFiltered() throws Exception {
+
+		mvc.perform(get("/api/searchVenues?near=Pune india&intent=browse&radius=10000&limit=10&query=KFC&filter=BBQ Joint")
+				.contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.meta").exists())
+                .andExpect(jsonPath("$.response").exists())
+                .andExpect(jsonPath("$.response.venues").exists())
+                .andExpect(jsonPath("$.response.venues").isArray())
+                .andExpect(jsonPath("$.response.venues[0].categories").isArray())
+                .andExpect(jsonPath("$.response.venues[0].categories[0].name").value("BBQ Joint"))
+                .andExpect(jsonPath("$.response.venues[0].venueDetails").exists())
+                .andExpect(jsonPath("$.response.venues[0].venueDetails.meta.code").value(200))
+                .andExpect(jsonPath("$.response.venues[0].venueDetails.response").isNotEmpty());
 	}
 
 }
--- src/test/java/com/location/LocationsApplicationTests.java
+++ src/test/java/com/location/LocationsApplicationTests.java
@@ -1,16 +1,25 @@
 package com.location;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.location.controller.LocationResource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LocationsApplicationTests {
 
+	@Autowired
+	LocationResource controller; 
+
 	@Test
 	public void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
